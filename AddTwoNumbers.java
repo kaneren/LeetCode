8fc1fb617d4db75bc504563c0d83f089932b1ddc
Kane Ren <rrkaige@gmail.com>
2016-08-02 22:00:34 +0800

    AddTwoNumbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,12 +3,9 @@ package com.rrkaige.leetcode;
 import java.util.Scanner;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * } 
+ * Example:
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
  * @author kaneren
  *
  */
