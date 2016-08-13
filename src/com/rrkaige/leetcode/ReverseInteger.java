ba9ec665dda7b4f36970485c5a95f13cd45936fb
kaneren <rrkaige@gmail.com>
2016-08-13 23:00:57 +0800

    leetcode

---
+++ src/com/rrkaige/leetcode/ReverseInteger.java
@@ -0,0 +1,32 @@
+package com.rrkaige.leetcode;
+/**
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ * @author kaneren
+ *
+ */
+public class ReverseInteger {
+    public static int reverse(int x) {
+    	if(x == 0 || x == Integer.MIN_VALUE) {
+    		return 0;
+    	}
+    	if(x < 0) {
+    		return -reverse(-x);
+    	}
+    	int res = 0;
+    	while(x != 0) {
+    		if(res > (Integer.MAX_VALUE - x % 10) / 10) {
+    			return 0;
+    		}
+    		res = res * 10 + x % 10;
+    		x /= 10;
+    	}
+    	return res;
+    }
+    
+	public static void main(String[] args) {
+		int x = 123;
+		int y = -123;
+		System.out.println(reverse(x)+","+reverse(y));
+	}
+}
